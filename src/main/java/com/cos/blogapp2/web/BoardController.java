9c6f9572d54bd75e1e745746ce37f048b878065d
Sleeping-Suit <hrcho721@gmail.com>
2021-09-17 11:27:34 +0900

    Board, User Controller href 설정

--- src/main/java/com/cos/blogapp2/web/BoardController.java
+++ src/main/java/com/cos/blogapp2/web/BoardController.java
@@ -2,6 +2,7 @@ package com.cos.blogapp2.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -9,5 +10,15 @@ public class BoardController {
 	@GetMapping("/board")
 	public String list() {
 		return "board/list";
+	}
+	
+	@GetMapping("/board/{id}")
+	public String detail(@PathVariable int id) {
+		return "board/detail";
+	}
+	
+	@GetMapping("/board/saveFrom")
+	public String boardSaveForm() {
+		return "board/saveForm";
 	}
 }
